cc8c48478c3467a028939a13a05b56684cf7d557
maxberger <maxberger@localhost>
2007-06-13 12:06:31 +0000

    Implemented ImageConverter

--- jeuclid-xxe/src/main/java/net/sourceforge/jeuclid/xxe/JEuclidImageToolkit.java
+++ jeuclid-xxe/src/main/java/net/sourceforge/jeuclid/xxe/JEuclidImageToolkit.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2007 - 2007 JEuclid, http://jeuclid.sf.net
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* $Id$ */
+
 package net.sourceforge.jeuclid.xxe;
 
 import java.io.File;
@@ -9,10 +27,15 @@ import com.xmlmind.xmledit.imagetoolkit.ImageConverter;
 import com.xmlmind.xmledit.imagetoolkit.ImageRenderer;
 import com.xmlmind.xmledit.imagetoolkit.ImageToolkit;
 
+/**
+ * Implements an ImageToolkit for XXE.
+ * 
+ * @author Max Berger
+ * @version $Revision$
+ */
 public class JEuclidImageToolkit implements ImageToolkit {
 
-// public static PrintStream DEBUG;
-
+// private static PrintStream DEBUG;
 // static {
 // try {
 // JEuclidImageToolkit.DEBUG = new PrintStream("/tmp/Mylog", "UTF-8");
@@ -21,6 +44,13 @@ public class JEuclidImageToolkit implements ImageToolkit {
 // }
 // }
 
+    /**
+     * Default constructor.
+     */
+    public JEuclidImageToolkit() {
+        // Empty on purpose.
+    }
+
     /** {@inheritDoc} */
     public String getDescription() {
         // JEuclidImageToolkit.DEBUG.println("getDescription");
@@ -28,19 +58,27 @@ public class JEuclidImageToolkit implements ImageToolkit {
                 + "Please see http://jeuclid.sf.net/ for more information.";
     }
 
+    private boolean hasRightExtension(final String filename) {
+        final String extension = filename.substring(
+                filename.lastIndexOf('.') + 1).toLowerCase(Locale.ENGLISH);
+        return "mml".equals(extension) || "odf".equals(extension);
+
+    }
+
     /** {@inheritDoc} */
-    public ImageConverter getImageConverter(final File arg0, final File arg1) {
-        // JEuclidImageToolkit.DEBUG.println("getImageConverter");
-        return null;
+    public ImageConverter getImageConverter(final File inFile,
+            final File outFile) {
+        if (this.hasRightExtension(inFile.getName())) {
+            return JEuclidImageConverter.getConverter();
+        } else {
+            return null;
+        }
     }
 
     /** {@inheritDoc} */
     public ImageRenderer getImageRenderer(final URL url) {
         final String path = url.getPath();
-        final String extension = path.substring(path.lastIndexOf('.') + 1)
-                .toLowerCase(Locale.ENGLISH);
-
-        if ("mml".equals(extension) || "odf".equals(extension)) {
+        if (this.hasRightExtension(path)) {
             return JEuclidImageRenderer.getRenderer();
         } else {
             return null;
