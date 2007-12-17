3c2205a7246ad1d52c805a48403d7b6b7eb83613
maxberger <maxberger@localhost>
2007-12-17 09:07:43 +0000

    Moved 1.6-dependent code to jeuclid-core16

---
+++ jeuclid-core16/src/main/java/net/sourceforge/jeuclid/swing/MathComponentUI16.java
@@ -0,0 +1,54 @@
+package net.sourceforge.jeuclid.swing;
+
+import java.awt.Component;
+import java.awt.Component.BaselineResizeBehavior;
+
+import javax.swing.JComponent;
+import javax.swing.SwingConstants;
+
+import net.sourceforge.jeuclid.swing.JMathComponent;
+import net.sourceforge.jeuclid.swing.MathComponentUI;
+
+/*
+ * Copyright 2002 - 2007 JEuclid, http://jeuclid.sf.net
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* $Id: MathComponentUI16.java $ */
+
+/**
+ * @version $Revision: 000 $
+ */
+public class MathComponentUI16 extends MathComponentUI {
+    /**
+     * Default Constructor.
+     */
+    public MathComponentUI16() {
+        super();
+    }
+
+    @Override
+    public BaselineResizeBehavior getBaselineResizeBehavior(final JComponent c) {
+        switch (((JMathComponent) c).getVerticalAlignment()) {
+        case SwingConstants.TOP:
+            return Component.BaselineResizeBehavior.CONSTANT_ASCENT;
+        case SwingConstants.BOTTOM:
+            return Component.BaselineResizeBehavior.CONSTANT_DESCENT;
+        case SwingConstants.CENTER:
+            return Component.BaselineResizeBehavior.CENTER_OFFSET;
+        }
+        return Component.BaselineResizeBehavior.OTHER;
+    }
+
+}
---
+++ jeuclid-core16/src/test/java/net/sourceforge/jeuclid/test/MathComponentUI16Test.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2002 - 2007 JEuclid, http://jeuclid.sf.net
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* $Id: MathComponentTest.java 517 2007-09-13 19:22:07Z maxberger $ */
+
+package net.sourceforge.jeuclid.test;
+
+import java.awt.HeadlessException;
+
+import net.sourceforge.jeuclid.swing.JMathComponent;
+import net.sourceforge.jeuclid.swing.MathComponentUI;
+import net.sourceforge.jeuclid.swing.MathComponentUI16;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Tests the MathComponentUI16
+ * 
+ * @version $Revision: 517 $
+ */
+public class MathComponentUI16Test {
+
+    @Test
+    public void testComponentUI16() throws Exception {
+        try {
+            final JMathComponent jm = new JMathComponent();
+            final MathComponentUI mcui = jm.getUI();
+            Assert.assertTrue(mcui instanceof MathComponentUI16);
+        } catch (final HeadlessException he) {
+            // Ignore to make test work on servers.
+        }
+    }
+
+}
