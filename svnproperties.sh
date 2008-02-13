5bee0d763e5aea24ebe90a534ffb8bae235d0f5f
maxberger <maxberger@localhost>
2008-02-13 11:17:17 +0000

    Fixed SVN Properties

--- svnproperties.sh
+++ svnproperties.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 find . -name \*.java -exec svn ps svn:keywords "Revision Date Id" '{}' \;
-find . -name \*.java -or -name \*.xsl -or -name \*.xml -exec svn ps svn:eol-style native '{}' \;
+find . -name \*.java -exeec svn ps svn:eol-style native '{}' \;
+find . -name \*.xsl  -exec svn ps svn:eol-style native '{}' \;
+find . -name \*.xml  -exec svn ps svn:eol-style native '{}' \;
