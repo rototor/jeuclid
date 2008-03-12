fcaa6c3dd3ac3517dfe5d4323b6fae9f0600aa0e
maxberger <maxberger@localhost>
2008-03-12 09:41:12 +0000

    More verbose

--- jeuclid-core/src/main/script/precompile.groovy
+++ jeuclid-core/src/main/script/precompile.groovy
@@ -12,13 +12,14 @@ import net.sourceforge.jeuclid.elements.support.operatordict.OperatorDictionary;
 
 File basedir = new File("${project.basedir}");
 
-// Precompile XSLT Stylesheet
+log.info("Precompiling XSLT stylesheet...");
 try {
   Source stylesheet = new StreamSource(new File(basedir,"target/classes/content/mathmlc2p.xsl"))
   TransformerFactory factory = TransformerFactory.newInstance();
   factory.setAttribute("generate-translet", Boolean.TRUE);
   factory.setAttribute("package-name", "translet");
-  factory.setAttribute("destination-directory", "${project.basedir}/target/classes");
+  File destDir = new File(basedir,"target/classes");
+  factory.setAttribute("destination-directory", destDir.getAbsolutePath());
   Templates templates = factory.newTemplates(stylesheet);
 
   OutputStream os = new FileOutputStream(new File(basedir,"target/classes/content/mathmlc2p.ser"));
@@ -26,10 +27,11 @@ try {
   oo.writeObject(templates);
   oo.close();
 } catch (Exception e) {
-  log.info("Could not precompile stylesheet");
+  log.info("Could not precompile stylesheet (optional step)");
+  log.info("Reason: "+e.getMessage());
 }
 
-// Preload operator dictionary
+log.info("Preloading operator dictionary...");
 Object dict = OperatorDictionary.getInstance().getDict();
 os = new FileOutputStream(new File(basedir,"target/classes/moDictionary.ser"));
 oo = new ObjectOutputStream(os);
