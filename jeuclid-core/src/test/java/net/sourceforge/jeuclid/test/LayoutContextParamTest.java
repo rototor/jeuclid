f35b090876adf20c517f5625b7fccc39273ce4b6
maxberger <maxberger@localhost>
2008-04-20 11:16:31 +0000

    Moved foprep into core, mml2xx into cli

--- jeuclid-core/src/test/java/net/sourceforge/jeuclid/test/LayoutContextParamTest.java
+++ jeuclid-core/src/test/java/net/sourceforge/jeuclid/test/LayoutContextParamTest.java
@@ -40,7 +40,7 @@ public class LayoutContextParamTest {
     public LayoutContextParamTest() {
         // Empty on purpose
     }
-    
+
     /**
      * Tests implementation of {@link Parameter#valid(Object)} method.
      */
@@ -50,7 +50,8 @@ public class LayoutContextParamTest {
         Assert.assertFalse(Parameter.DEBUG.valid("true"));
         Assert.assertTrue(Parameter.DISPLAY.valid(Display.BLOCK));
         Assert.assertFalse(Parameter.DISPLAY.valid(null));
-        Assert.assertTrue(Parameter.FONTS_SERIF.valid(Collections.singletonList("Arial")));
+        Assert.assertTrue(Parameter.FONTS_SERIF.valid(Collections
+                .singletonList("Arial")));
         Assert.assertFalse(Parameter.FONTS_SERIF.valid("Courier"));
         Assert.assertTrue(Parameter.MATHBACKGROUND.valid(Color.RED));
         Assert.assertTrue(Parameter.MATHBACKGROUND.valid(null));
@@ -59,7 +60,7 @@ public class LayoutContextParamTest {
         Assert.assertTrue(Parameter.SCRIPTLEVEL.valid(new Integer(1)));
         Assert.assertFalse(Parameter.SCRIPTLEVEL.valid(new Float(1)));
     }
-    
+
     /**
      * Tests implementation of {@link Parameter#fromString(String)} method.
      */
@@ -67,28 +68,34 @@ public class LayoutContextParamTest {
     public void testFromString() {
         Assert.assertEquals(Parameter.DEBUG.fromString("true"), Boolean.TRUE);
         Assert.assertEquals(Parameter.DEBUG.fromString(null), null);
-        Assert.assertEquals(Parameter.DISPLAY.fromString("BLOCK"), Display.BLOCK);
+        Assert.assertEquals(Parameter.DISPLAY.fromString("BLOCK"),
+                Display.BLOCK);
         try {
             Parameter.DISPLAY.fromString("foo");
             Assert.fail();
-        } catch (IllegalArgumentException e) {}
+        } catch (final IllegalArgumentException e) {
+        }
         Assert.assertEquals(Parameter.FONTS_SERIF.fromString("Arial"),
                 Collections.singletonList("Arial"));
-        Assert.assertEquals(Parameter.FONTS_SERIF.fromString("Arial,Helvetica"),
-                Arrays.asList(new String[] {"Arial", "Helvetica"}));
+        Assert.assertEquals(Parameter.FONTS_SERIF
+                .fromString("Arial,Helvetica"), Arrays.asList(new String[] {
+                "Arial", "Helvetica" }));
         Assert.assertEquals(Parameter.FONTS_SERIF.fromString("Foo, Bar"),
-                Arrays.asList(new String[] {"Foo", "Bar"}));
+                Arrays.asList(new String[] { "Foo", "Bar" }));
         Assert.assertEquals(Parameter.MATHCOLOR.fromString("RED"), Color.RED);
         try {
             Parameter.MATHCOLOR.fromString("violet");
             Assert.fail();
-        } catch (IllegalArgumentException e) {}
-        Assert.assertEquals(Parameter.MATHCOLOR.fromString("rgb(100,100,100)"), 
-                new Color(100, 100, 100));
-        Assert.assertEquals(Parameter.MATHSIZE.fromString("0.5"), new Float(0.5));
-        Assert.assertEquals(Parameter.SCRIPTLEVEL.fromString("1"), new Integer(1));
+        } catch (final IllegalArgumentException e) {
+        }
+        Assert.assertEquals(Parameter.MATHCOLOR
+                .fromString("rgb(100,100,100)"), new Color(100, 100, 100));
+        Assert.assertEquals(Parameter.MATHSIZE.fromString("0.5"), new Float(
+                0.5));
+        Assert.assertEquals(Parameter.SCRIPTLEVEL.fromString("1"),
+                new Integer(1));
     }
-    
+
     /**
      * Tests implementation of {@link Parameter#toString(String)} method.
      */
@@ -96,15 +103,19 @@ public class LayoutContextParamTest {
     public void testToString() {
         Assert.assertEquals(Parameter.DEBUG.toString(Boolean.TRUE), "true");
         Assert.assertEquals(Parameter.DEBUG.toString(null), null);
-        Assert.assertEquals(Parameter.DISPLAY.toString(Display.INLINE), "INLINE");
-        Assert.assertEquals(Parameter.FONTS_SERIF.toString(
-                Collections.singletonList("font")), "font");
-        Assert.assertEquals(Parameter.FONTS_SERIF.toString(
-                Arrays.asList(new String[] {"f1", "f2"})), "f1,f2");
+        Assert.assertEquals(Parameter.DISPLAY.toString(Display.INLINE),
+                "INLINE");
+        Assert.assertEquals(Parameter.FONTS_SERIF.toString(Collections
+                .singletonList("font")), "font");
+        Assert.assertEquals(Parameter.FONTS_SERIF.toString(Arrays
+                .asList(new String[] { "f1", "f2" })), "f1, f2");
         Assert.assertEquals(Parameter.MATHCOLOR.toString(Color.RED), "red");
         Assert.assertEquals(Parameter.MATHCOLOR.toString(null), null);
-        Assert.assertEquals(Parameter.MATHCOLOR.toString(new Color(32, 32, 32)), "#202020");
-        Assert.assertEquals(Parameter.MATHSIZE.toString(new Float(0.5)), "0.5");
-        Assert.assertEquals(Parameter.SCRIPTLEVEL.toString(new Integer(2)), "2");
-    }    
+        Assert.assertEquals(Parameter.MATHCOLOR
+                .toString(new Color(32, 32, 32)), "#202020");
+        Assert.assertEquals(Parameter.MATHSIZE.toString(new Float(0.5)),
+                "0.5");
+        Assert.assertEquals(Parameter.SCRIPTLEVEL.toString(new Integer(2)),
+                "2");
+    }
 }
