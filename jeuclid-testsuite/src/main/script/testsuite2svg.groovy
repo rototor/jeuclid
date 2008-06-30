3745653115e1ad7ba269a939619b54c38909207a
maxberger <maxberger@localhost>
2008-06-30 13:04:30 +0000

    Render Testsuite with font-size 16

--- jeuclid-testsuite/src/main/script/testsuite2svg.groovy
+++ jeuclid-testsuite/src/main/script/testsuite2svg.groovy
@@ -3,6 +3,8 @@ import java.io.FileOutputStream;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
+import net.sourceforge.jeuclid.*
+import net.sourceforge.jeuclid.context.*;
 import net.sourceforge.jeuclid.converter.Processor;
 
 log.info("Converting testsuite...");
@@ -16,6 +18,8 @@ scanner = ant.fileScanner {
 def target = "target/site/svg"
 
 Processor p = Processor.getInstance();
+MutableLayoutContext l = LayoutContextImpl.getDefaultLayoutContext()
+l.setParameter(Parameter.MATHSIZE, 16)
 for (f in scanner) {
   f2 = new File(f.getPath().replace("src/main/resources","target/site/svg"))
   ant.mkdir(dir: f2.getParent())
@@ -26,7 +30,7 @@ for (f in scanner) {
     def inputSource = new StreamSource(f)
     def result = new StreamResult(f2)
     try {
-      p.process(inputSource, result);
+      p.process(inputSource, result, l);
       processed = true;
     } catch (final TransformerException te) {
       processed = false;
