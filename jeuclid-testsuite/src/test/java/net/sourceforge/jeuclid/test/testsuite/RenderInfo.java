150483fb4c6d110de729789c12e02f30b77aefa8
maxberger <maxberger@localhost>
2009-01-28 09:39:55 +0000

    JavaDoc Fixes

--- jeuclid-testsuite/src/test/java/net/sourceforge/jeuclid/test/testsuite/RenderInfo.java
+++ jeuclid-testsuite/src/test/java/net/sourceforge/jeuclid/test/testsuite/RenderInfo.java
@@ -1,7 +1,30 @@
+/*
+ * Copyright 2008 - 2009 JEuclid, http://jeuclid.sf.net
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* $Id: JEuclidView.java 795 2008-06-21 10:53:35Z maxberger $ */
+
 package net.sourceforge.jeuclid.test.testsuite;
 
 import java.io.Serializable;
 
+/**
+ * Storable Render Information, to compare layout.
+ * 
+ * @version $Revision: 000 $
+ */
 public class RenderInfo implements Serializable {
     private static final float FUZZYNESS = 0.1f;
 
@@ -22,6 +45,22 @@ public class RenderInfo implements Serializable {
 
     private final float posy;
 
+    /**
+     * Default constructor.
+     * 
+     * @param elementName
+     *            Name of the element
+     * @param ascent
+     *            Ascent height
+     * @param descent
+     *            Descent height
+     * @param width
+     *            Width
+     * @param posx
+     *            X-Position
+     * @param posy
+     *            Y-Position.
+     */
     public RenderInfo(final String elementName, final float ascent,
             final float descent, final float width, final float posx,
             final float posy) {
@@ -38,6 +77,14 @@ public class RenderInfo implements Serializable {
         this.posy = posy;
     }
 
+    /**
+     * Checks if this render information is similar enough (within
+     * {@link #FUZZYNESS} to another RenderInfo object.
+     * 
+     * @param other
+     *            RenderInfo to compare to.
+     * @return true if both RenderInfos are similar.
+     */
     public boolean isSimilar(final RenderInfo other) {
         boolean isSim = true;
         isSim &= (this.elementName.equals(other.elementName));
@@ -55,6 +102,9 @@ public class RenderInfo implements Serializable {
         return Math.abs(should - is) <= (maxdelta);
     }
 
+    /**
+     * @return the Element Name.
+     */
     public String getElementName() {
         return this.elementName;
     }
