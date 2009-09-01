847dfcc4c4c239fe6ff8d075cca1805e98ce1149
Max Berger <max@berger.name>
2009-09-01 11:06:26 +0200

    Created common TestSuiteProcessor

--- jeuclid-testsuite/src/main/java/net/sourceforge/jeuclid/testsuite/Servlet3.java
+++ jeuclid-testsuite/src/main/java/net/sourceforge/jeuclid/testsuite/Servlet3.java
@@ -28,22 +28,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.transform.Result;
 import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMResult;
-import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
-
-import net.sourceforge.jeuclid.DOMBuilder;
-import net.sourceforge.jeuclid.MutableLayoutContext;
-import net.sourceforge.jeuclid.context.LayoutContextImpl;
-import net.sourceforge.jeuclid.context.Parameter;
-import net.sourceforge.jeuclid.converter.Processor;
-
-import org.w3c.dom.Node;
 
 /**
  * Serve the W3C MathML Testsuite v3, rendering all formulas with JEuclid to
@@ -53,43 +39,20 @@ import org.w3c.dom.Node;
  */
 public class Servlet3 extends HttpServlet {
 
-    // /**
-    // * Logger for this class
-    // */
-    // private static final Log LOGGER = LogFactory.getLog(Servlet3.class);
-
     private static final int BLOCK_SIZE = 4096;
-
-    private static final float DISPLAY_SIZE = 16.0f;
 
     /**
      * 
      */
     private static final long serialVersionUID = 1L;
 
-    private static final Processor MML2SVGPROCESSOR = Processor.getInstance();
-
-    private final Transformer modificationTransformer;
-
-    private final MutableLayoutContext context;
+    private static final TestSuiteProcessor TSP = TestSuiteProcessor
+            .getInstance();
 
     /**
      * Default Constructor.
      */
     public Servlet3() {
-        this.context = new LayoutContextImpl(LayoutContextImpl
-                .getDefaultLayoutContext());
-        this.context.setParameter(Parameter.MATHSIZE, Servlet3.DISPLAY_SIZE);
-        Transformer t;
-        try {
-            t = TransformerFactory.newInstance().newTemplates(
-                    new StreamSource(DOMBuilder.class
-                            .getResourceAsStream("/support/ModifySuite3.xsl")))
-                    .newTransformer();
-        } catch (final TransformerConfigurationException e) {
-            t = null;
-        }
-        this.modificationTransformer = t;
     }
 
     /** {@inheritDoc} */
@@ -107,9 +70,10 @@ public class Servlet3 extends HttpServlet {
             final OutputStream out = resp.getOutputStream();
 
             boolean processed = false;
-            if (file.endsWith(".xhtml")
-                    && (this.modificationTransformer != null)) {
-                processed = this.processDocument(stream, out);
+            if (file.endsWith(".xhtml")) {
+                final Source inputSource = new StreamSource(stream);
+                final Result result = new StreamResult(out);
+                processed = Servlet3.TSP.process(inputSource, result, true);
             }
             if (!processed) {
                 final byte[] buf = new byte[Servlet3.BLOCK_SIZE];
@@ -120,29 +84,6 @@ public class Servlet3 extends HttpServlet {
                 }
             }
         }
-    }
-
-    /**
-     * @param stream
-     * @param out
-     * @param processed
-     * @return
-     */
-    private boolean processDocument(final InputStream stream,
-            final OutputStream out) {
-        boolean processed;
-        final Source inputSource = new StreamSource(stream);
-        final DOMResult intermediate = new javax.xml.transform.dom.DOMResult();
-        final Result result = new StreamResult(out);
-        try {
-            Servlet3.MML2SVGPROCESSOR.process(inputSource, intermediate);
-            final Node head = intermediate.getNode();
-            this.modificationTransformer.transform(new DOMSource(head), result);
-            processed = true;
-        } catch (final TransformerException te) {
-            processed = false;
-        }
-        return processed;
     }
 
 }
---
+++ jeuclid-testsuite/src/main/java/net/sourceforge/jeuclid/testsuite/TestSuiteProcessor.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2009 - 2009 JEuclid, http://jeuclid.sf.net
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* $Id$ */
+
+package net.sourceforge.jeuclid.testsuite;
+
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMResult;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamSource;
+
+import net.sourceforge.jeuclid.DOMBuilder;
+import net.sourceforge.jeuclid.MutableLayoutContext;
+import net.sourceforge.jeuclid.context.LayoutContextImpl;
+import net.sourceforge.jeuclid.context.Parameter;
+import net.sourceforge.jeuclid.converter.Processor;
+
+import org.w3c.dom.Node;
+
+/**
+ * Process the actual TestSuite.
+ * 
+ * @version $Revision$
+ */
+public final class TestSuiteProcessor {
+    private static final float DISPLAY_SIZE = 16.0f;
+    private static final Processor MML2SVGPROCESSOR = Processor.getInstance();
+
+    private final Transformer modificationTransformer;
+    private final MutableLayoutContext context;
+
+    private static final class SingletonHolder {
+        private static final TestSuiteProcessor INSTANCE = new TestSuiteProcessor();
+
+        private SingletonHolder() {
+        }
+    }
+
+    private TestSuiteProcessor() {
+        this.context = new LayoutContextImpl(LayoutContextImpl
+                .getDefaultLayoutContext());
+        this.context.setParameter(Parameter.MATHSIZE,
+                TestSuiteProcessor.DISPLAY_SIZE);
+        Transformer t;
+        try {
+            t = TransformerFactory.newInstance().newTemplates(
+                    new StreamSource(DOMBuilder.class
+                            .getResourceAsStream("/support/ModifySuite3.xsl")))
+                    .newTransformer();
+        } catch (final TransformerConfigurationException e) {
+            t = null;
+        }
+        this.modificationTransformer = t;
+    }
+
+    /**
+     * Retrieve the TestSuiteProcessor singleton object.
+     * 
+     * @return the TestSuiteProcessor.
+     */
+    public static TestSuiteProcessor getInstance() {
+        return TestSuiteProcessor.SingletonHolder.INSTANCE;
+    }
+
+    /**
+     * Process Source testsuite xhtml file to xhtml.
+     * 
+     * @param inputSource
+     *            InputSource
+     * @param result
+     *            Result
+     * @param apply3Mod
+     *            if true apply modifications specific to testsuite3
+     * @return true if something was written to result.
+     */
+    public boolean process(final Source inputSource, final Result result,
+            final boolean apply3Mod) {
+        boolean processed;
+        final DOMResult intermediate = new javax.xml.transform.dom.DOMResult();
+        try {
+            if (apply3Mod && (this.modificationTransformer != null)) {
+                TestSuiteProcessor.MML2SVGPROCESSOR.process(inputSource,
+                        intermediate);
+                final Node head = intermediate.getNode();
+                this.modificationTransformer.transform(new DOMSource(head),
+                        result);
+            } else {
+                TestSuiteProcessor.MML2SVGPROCESSOR
+                        .process(inputSource, result);
+            }
+            processed = true;
+        } catch (final TransformerException te) {
+            processed = false;
+        }
+        return processed;
+    }
+
+}
