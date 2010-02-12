5dcc76c8969a801e9e222546b30237567083ee10
Max Berger <max@berger.name>
2010-02-12 22:00:27 +0100

    Processing is now a task

--- jeuclid-testsuite/src/main/script/testsuite2svg.groovy
+++ jeuclid-testsuite/src/main/script/testsuite2svg.groovy
@@ -6,36 +6,64 @@ import javax.xml.transform.stream.StreamSource;
 import net.sourceforge.jeuclid.*
 import net.sourceforge.jeuclid.context.*;
 import net.sourceforge.jeuclid.testsuite.TestSuiteProcessor;
+import java.util.concurrent.*;
+
+class TheTask implements Runnable {
+
+  def f;
+  def tsp;
+  def source;
+  def target;
+  def ext;
+  def ant;
+  def mod3;
+
+  public TheTask(file,t,s,tg,e,a,m) {
+    this.f = file;
+    this.tsp = t;
+    this.source = s;
+    this.target = tg;
+    this.ext = e;
+    this.ant = a;
+    this.mod3 = m;
+  }
+  
+  public void run() {
+    def f2 = new File(f.getPath().replace(source,target))
+    ant.mkdir(dir: f2.getParent())
+
+    def processed = false;
+
+    if (f.getPath().endsWith(ext)) {
+      def inputSource = new StreamSource(f)
+      def result = new StreamResult(f2)
+      processed = tsp.process(inputSource,result,mod3)
+    }
+
+    if (!processed) {
+      ant.copy(file: f, tofile:f2)
+    }
+    
+  }
+}
+
 
 def doit(extension,sourcelast,targetlast,mod3) {
 
-def tsp = TestSuiteProcessor.getInstance();
-def source = "src/main/resources/"+sourcelast;
-def target = "target/site/"+targetlast;
-def ext = "."+extension;
+  def tsp = TestSuiteProcessor.getInstance();
+  def source = "src/main/resources/"+sourcelast;
+  def target = "target/site/"+targetlast;
+  def ext = "."+extension;
 
-scanner = ant.fileScanner {
-  fileset(dir:"${project.basedir}/"+source) {
-    include(name:"**/*")
-  }
-}
-
-for (f in scanner) {
-  f2 = new File(f.getPath().replace(source,target))
-  ant.mkdir(dir: f2.getParent())
-
-  def processed = false;
-
-  if (f.getPath().endsWith(ext)) {
-    def inputSource = new StreamSource(f)
-    def result = new StreamResult(f2)
-    processed = tsp.process(inputSource,result,mod3)
+  scanner = ant.fileScanner {
+    fileset(dir:"${project.basedir}/"+source) {
+      include(name:"**/*")
+    }
   }
 
-  if (!processed) {
-    ant.copy(file: f, tofile:f2)
+  for (f in scanner) {
+    new TheTask(f,tsp,source,target,ext,ant,mod3).run();
   }
-}
 
 }
 
